bc95cbfbb322ed30d3c2a8b93c53b88257d98240
Yeho Gwon <devstita@gmail.com>
2023-06-25 17:21:50 +0900

    wrap Bottleneck

--- model/resnet.py
+++ model/resnet.py
@@ -26,12 +26,28 @@ class Block(nn.Module):
         
         return self.relu(x + out)
 
+# FIXME: Bottleneck has tons of bugs, so not being used at the moment
 class Bottleneck(nn.Module): 
-    def __init__(self, dim: int, dim_change: bool=False) -> None: # dim_change is True when this block is the first block of a stage
+    def __init__(self, dim: int, first: bool=False, second: bool=False) -> None: 
         super().__init__()
 
-        scale = 2 if dim_change else 1
-        self.conv1 = nn.Conv2d(in_channels=int(dim / scale), out_channels=dim, kernel_size=1, stride=scale, bias=False)
+        # FIRST SECOND    SCALE
+        #   0     0   -> scale = 1
+        #   0     1   -> scale = 4
+        #   1     0   -> scale = 0.5
+        #   1     1   -> FORBIDDEN
+
+        both = first and second
+        assert not both, 'one of first and second should be True, not both'
+
+        if first: 
+            scale = 0.5
+        elif second: 
+            scale = 4
+        else:
+            scale = 1
+
+        self.conv1 = nn.Conv2d(in_channels=int(dim * scale), out_channels=dim, kernel_size=1, stride=scale, bias=False)
         self.bn1 = nn.BatchNorm2d(num_features=dim)
         self.conv2 = nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=3, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(num_features=dim)
@@ -39,16 +55,19 @@ class Bottleneck(nn.Module):
         self.bn3 = nn.BatchNorm2d(num_features=dim * 4)
         
         self.relu = nn.ReLU()
-        self._dim_change = nn.Conv2d(in_channels=int(dim / scale), out_channels=dim * 4, kernel_size=1, stride=2, bias=False) if dim_change else nn.Identity()
+        self._dim_change = nn.Conv2d(in_channels=int(dim * scale), out_channels=dim * 4, kernel_size=1, stride=scale, bias=False)
 
         # TODO: Initialize weights
     
-    def forward(self, x: torch.Tensor) -> torch.Tensor: 
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        print('!!! forward !!!') 
         out = self.relu(self.bn1(self.conv1(x)))
         out = self.relu(self.bn2(self.conv2(out)))
         out = self.bn3(self.conv3(out))
 
+        print('bef x:', x.shape, ', out:', out.shape)
         x = self._dim_change(x)
+        print('aft x:', x.shape, ', out:', out.shape)
         
         return self.relu(x + out)
 
@@ -58,24 +77,31 @@ class ResNet(nn.Module):
         super().__init__()
         
         assert type(sizes) == list and len(sizes) == 4, 'sizes should be a list of 4 integers'
+        assert not bottleneck, 'bottleneck is not supported yet'
 
         self.sizes = sizes
-        self.bottleneck = bottleneck
+        # self.bottleneck = bottleneck
 
-        # TODO: the value of paddings is not specified in the paper
+        # TODO: the value of paddings is not specified in the paper; it is set to match the output size shown in the paper
 
-        self.conv = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=7, stride=2, padding=1, bias=False)
+        self.conv = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False)
         self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.bn = nn.BatchNorm2d(num_features=16)
+        self.bn = nn.BatchNorm2d(num_features=64)
         self.relu = nn.ReLU()
 
-        BlockClass = Bottleneck if bottleneck else Block
+        # BlockClass = Bottleneck if bottleneck else Block
+        BlockClass = Block
 
         self.module_stack = [
-            nn.ModuleList([Block(dim=16, dim_change=False) for i in range(n)]), 
-            nn.ModuleList([Block(dim=32, dim_change=i==0) for i in range(n)]), 
-            nn.ModuleList([Block(dim=64, dim_change=i==0) for i in range(n)])
+            nn.Sequential(*[BlockClass(dim=64, dim_change=False) for i in range(sizes[0])]), 
+            nn.Sequential(*[BlockClass(dim=128, dim_change=i==0) for i in range(sizes[1])]), 
+            nn.Sequential(*[BlockClass(dim=256, dim_change=i==0) for i in range(sizes[2])]),
+            nn.Sequential(*[BlockClass(dim=512, dim_change=i==0) for i in range(sizes[3])])
         ]
+
+        # self.module_stack = [
+        #     nn.Sequential(*[BlockClass(dim=2 ** (i + 6), dim_change=i==0 and not (d == 0)) for i in range(sizes[d])])
+        # for d in range(4)]
 
         self.gap = nn.AdaptiveAvgPool2d(output_size=1000)
         self.fc = nn.Linear(in_features=1000, out_features=10, bias=True)
@@ -83,13 +109,12 @@ class ResNet(nn.Module):
         # TODO: Initialize weights
 
     def forward(self, x: torch.Tensor) -> torch.Tensor: 
-        out = self.relu(self.bn1(self.conv1(x)))
-        for ml in self.module_stack: 
-            for m in ml: 
-                out = m(out)
-        out = self.avg_pool(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
+        out = self.relu(self.bn(self.pool(self.conv(x))))
+        out = self.module_stack[0](out)
+        out = self.module_stack[1](out)
+        out = self.module_stack[2](out)
+        out = self.module_stack[3](out)
+        out = self.gap(out)
         return out
     
     def __repr__(self):

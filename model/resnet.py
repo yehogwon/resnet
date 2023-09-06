8eec6406325c9608ad17d6e79e63338f695b4686
Yeho Gwon <yeho.gwon@postech.ac.kr>
2023-09-06 14:25:15 +0900

    bottleneck block support

--- model/resnet.py
+++ model/resnet.py
@@ -1,5 +1,3 @@
-from typing import Optional, Type, Union, List
-
 import torch
 import torch.nn as nn
 
@@ -31,4 +29,27 @@ class BasicBlock(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor: 
         z = self.relu(self.bn1(self.conv1(x)))
         z = self.bn2(self.conv2(z))
+        return self.relu(z + self.shortcut(x))
+
+class Bottleneck(nn.Module): 
+    def __init__(self, in_channels: int, bottleneck_channels: int, out_channels: int, stride: int) -> None:
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, bottleneck_channels, kernel_size=1, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(bottleneck_channels)
+        self.conv2 = nn.Conv2d(bottleneck_channels, bottleneck_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(bottleneck_channels)
+        self.conv3 = nn.Conv2d(bottleneck_channels, out_channels, kernel_size=1, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+
+        if stride != 1 or in_channels != out_channels: 
+            self.shortcut = ShortcutProjection(in_channels, out_channels, stride)
+        else: 
+            self.shortcut = nn.Identity()
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        z = self.relu(self.bn1(self.conv1(x)))
+        z = self.relu(self.bn2(self.conv2(z)))
+        z = self.bn3(self.conv3(z))
         return self.relu(z + self.shortcut(x))
